bc05552bb72a6749318103c348bc9eec5ca3fadb
Igo Carvalho <igocarvalho00@gmail.com>
2022-11-18 07:11:27 -0300

    configure html webpack plugin

--- Chapter-I/fundamentos-do-react/src/index.jsx
+++ Chapter-I/fundamentos-do-react/src/index.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import {render} from "react-dom";
 import { App } from './App'
 
 function Main() {
   return <h1>Hello World!</h1>
 }
+
+render(<Main></Main>, document.getElementById('root'))
