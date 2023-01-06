26d01994aa078fd7c6812e3e7ea68343d85a756b
Igo Carvalho <igocarvalho00@gmail.com>
2023-01-06 03:43:02 -0300

    http requisitions

--- Chapter-I/fundamentos-do-react/src/App.jsx
+++ Chapter-I/fundamentos-do-react/src/App.jsx
@@ -1,11 +1,5 @@
-import { Counter } from "./components/Counter";
 import { RepositoryList } from "./components/RepositoryList";
 
 export function App() {
-  return (
-    <>
-      <RepositoryList />
-      <Counter />
-    </>
-  );
+  return <RepositoryList />;
 }
