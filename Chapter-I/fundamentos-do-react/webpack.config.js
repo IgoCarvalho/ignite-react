d7c2b6b551d4415cfbc54ce79c70bde629ca7213
Igo Carvalho <igocarvalho00@gmail.com>
2023-01-06 03:18:59 -0300

    configure React fast refresh

--- Chapter-I/fundamentos-do-react/webpack.config.js
+++ Chapter-I/fundamentos-do-react/webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
-  devtool: isDevelopment ? 'eval-source-map': 'source-map',
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -19,18 +20,29 @@ module.exports = {
       directory: path.resolve(__dirname, 'public', 'index.html')
     },
     compress: true,
+    hot: true,
   },
   plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html')
     })
-  ],
+  ].filter(Boolean),
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              plugins: [
+                isDevelopment && require.resolve('react-refresh/babel')
+              ].filter(Boolean)
+            }
+          }
+        ]
       },
       {
         test: /\.s[ac]ss$/i,
