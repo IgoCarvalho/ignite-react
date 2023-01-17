9abc35243f2d711d48e2e212ac1a1d670b0593d0
Igo Carvalho <igocarvalho00@gmail.com>
2023-01-17 20:17:40 -0300

    🤡 create mock seeds and post route

--- Chapter-II/primeira-aplicacao-com-react/src/main.tsx
+++ Chapter-II/primeira-aplicacao-com-react/src/main.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 
 import { App } from './App';
 
 createServer({
+  models: {
+    transactions: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Desenvolvimento de site',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2023-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Hambúrguer',
+          type: 'withdraw',
+          category: 'Comida',
+          amount: 30,
+          createdAt: new Date('2023-02-13 20:00:00'),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Ola',
-          amount: 200,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date(),
-        },
-      ];
+      return this.schema.all('transactions');
+    });
+
+    this.post('transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transactions', data);
     });
   },
 });
